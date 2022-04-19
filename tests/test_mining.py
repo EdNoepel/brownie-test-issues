c35ab9a415f27f1a1233125e2aa6d46516d332eb
Ed Noepel <ed@ajna.finance>
2022-04-19 12:18:05 -0400

    clarify that a non-default number of blocks still works

--- tests/test_mining.py
+++ tests/test_mining.py
@@ -5,7 +5,7 @@ def test_mine(chain, contract):
     chain.sleep(14)                         # this works with Ganache and Hardhat
     contract.doSomething({"from": accounts[0]})
 
-    chain.mine(blocks=1)                    # this works with Ganache and Hardhat
+    chain.mine(blocks=2)                    # this works with Ganache and Hardhat
     contract.doSomething({"from": accounts[0]})
 
     chain.mine(blocks=2, timedelta=28)      # this works with Ganache but not Hardhat
