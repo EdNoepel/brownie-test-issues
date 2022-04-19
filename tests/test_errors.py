037be2738280f3c170e7097d0340b78fe0440d13
Ed Noepel <ed@ajna.finance>
2022-04-19 12:07:47 -0400

    workaround for trapping custom errors

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -1,5 +1,7 @@
 import brownie
+import pytest
 from brownie.exceptions import VirtualMachineError
+from web3 import Web3
 
 
 def test_require(contract):
@@ -15,9 +17,23 @@ def test_custom_error(contract):
         assert "CustomError" in str(ex)
 
 
+def test_custom_error_workaround(contract):
+    # thanks to https://github.com/eth-brownie/brownie/issues/1108#issuecomment-937741398
+    expected_revert_string = "typed error: " + Web3.keccak(text="CustomError()")[:4].hex()
+    with brownie.reverts(expected_revert_string):
+        contract.testCustomError()
+
+
 def test_custom_error_in_library(contract):
     tx = None
     try:
         tx = contract.testCustomErrorInLibrary()
     except VirtualMachineError as ex:
         assert "CustomLibraryError" in str(ex)
+
+
+def test_custom_error_workaround_in_library(contract):
+    # thanks to https://github.com/eth-brownie/brownie/issues/1108#issuecomment-937741398
+    expected_revert_string = "typed error: " + Web3.keccak(text="CustomLibraryError()")[:4].hex()
+    with brownie.reverts(expected_revert_string):
+        contract.testCustomErrorInLibrary()
